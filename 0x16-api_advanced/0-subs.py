b7663fe3ffa64c2769d0ba56ca3e60daeb405578
Mery9yuka <benhaida.maryem@gmail.com>
2024-05-07 08:57:38 -0700

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the nb of subscribers for a given subreddit"""
-       Url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    Url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
             "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
             }

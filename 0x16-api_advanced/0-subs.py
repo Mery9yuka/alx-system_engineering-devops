c772c9d4eb124d6d7f0e452c563d16cef426935f
Mery9yuka <benhaida.maryem@gmail.com>
2024-05-07 08:48:16 -0700

    api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the nb of subscribers for a given subreddit"""
+     resp = requests.get(
+             "https://www.reddit.com/r/{}/about.json".format(subreddit),
+             Headers={"User-Agent": "Custom"},
+             )
+
+     if resp.status_code == 200:
+         return resp.json().get("data").get("subscribers")
+    else:
+        return 0

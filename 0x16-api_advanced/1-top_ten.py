1283cb5741ae97931133f341b6a571fff1d03610
Mery9yuka <benhaida.maryem@gmail.com>
2024-05-08 01:24:17 +0000

    api adv

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts
+Function that queries the Reddit API 
+& prints the titles of the first 10 hot posts
 """
 
 import requests
@@ -9,14 +9,14 @@ import requests
 
 def top_ten(subreddit):
     """
-    Function that querie the Reddit
+    Function that querie the Reddit API
     """
     Req = requests.get(
         "https://www.reddit.com/r/{}/hot.json".format(subreddit),
         headers={"User-Agent": "Custom"},
         p={"limit": 10},
     )
-     if Req.status_code == 200:
+    if Req.status_code == 200:
         for get_d in Req.json().get("data").get("children"):
             data = get_d.get("data")
             t = data.get("title")

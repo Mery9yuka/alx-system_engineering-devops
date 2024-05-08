6854b73b8228f5118e4077ede6385545c849ed11
Mery9yuka <benhaida.maryem@gmail.com>
2024-05-08 01:16:54 +0000

    api adv

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the nb of subscribers for a given subreddit"""
+    """Return the nb of subscribers for a given subreddit
+       returns the number of subscribers for a given subreddit. 
+       If an invalid subreddit is given, the function should return 0.    
+    """
     Url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
             "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"

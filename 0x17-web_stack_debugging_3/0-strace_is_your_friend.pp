86492625c3f7c8d2b890204fa086efff6fbad292
Mery9yuka <benhaida.maryem@gmail.com>
2024-06-05 01:54:30 +0000

    up

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# automated & fix using puppet to know why Apache is returning a 500 error
-
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
-}
